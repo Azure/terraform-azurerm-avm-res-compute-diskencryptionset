eaf1dcb1e547faaa65389c506f21df2e0b011c1b
Akashc0807 <akashc@microsoft.com>
2024-08-14 15:45:20 +0530

    add: DIsk Encryption set

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,16 @@
-output "private_endpoints" {
-  description = <<DESCRIPTION
-  A map of the private endpoints created.
-  DESCRIPTION
-  value       = var.private_endpoints_manage_dns_zone_group ? azurerm_private_endpoint.this_managed_dns_zone_groups : azurerm_private_endpoint.this_unmanaged_dns_zone_groups
+output "key_vault_key_url" {
+  description = "The ID of the disk encryption set."
+  value       = azurerm_disk_encryption_set.this.key_vault_key_url
 }
+
+output "resource" {
+  description = "This is the full output for the resource."
+  value       = azurerm_disk_encryption_set.this
+}
+
+output "resource_id" {
+  description = "The ID of the disk encryption set."
+  value       = azurerm_disk_encryption_set.this.id
+}
+
+# https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
