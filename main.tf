d173ceb59f6a3e39b1c76a55d3c34770506005bb
Akashc0807 <akashc@microsoft.com>
2024-08-15 18:18:19 +0530

    update: Updating mangement lock.

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_management_lock" "this" {
 
   lock_level = var.lock.kind
   name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
-  scope      = azurerm_MY_RESOURCE.this.id # TODO: Replace with your azurerm resource name
+  scope      = azurerm_disk_encryption_set.this.id # TODO: Replace with your azurerm resource name
   notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete the resource or its child resources." : "Cannot delete or modify the resource or its child resources."
 }
 
