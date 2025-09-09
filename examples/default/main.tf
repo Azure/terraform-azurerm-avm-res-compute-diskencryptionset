a05662ffb0c4578f8c8b41df5816aa2d984f91db
Akash Choudhary <114410741+Akashc0807@users.noreply.github.com>
2025-09-09 12:49:46 +0530

    Updating keys

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -4,10 +4,6 @@ terraform {
   required_version = "~> 1.9"
 
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = ">=1.9.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.71"
