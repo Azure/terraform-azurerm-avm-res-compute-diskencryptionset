a27392e466432f93410b14b121e09732c4abaf0f
Akash Choudhary <114410741+Akashc0807@users.noreply.github.com>
2025-09-09 12:04:03 +0530

    Updating azapi version in examples.

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -4,10 +4,6 @@ terraform {
   required_version = "~> 1.9"
 
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "~> 2.4"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.71"
